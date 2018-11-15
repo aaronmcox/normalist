c1fdfe157e263d55f425e2784dae307b1b30bc0d
Aaron Cox <i.aaroncox@gmail.com>
2018-11-14 22:21:53 -0500

    refactor create definition, test extraData parameter

--- create.js
+++ create.js
@@ -1,8 +1,8 @@
 
-export default (array, idKey = "id", otherData) => array.reduce((associalist, element) => {
+export default (array, idKey = "id", extraData = {}) => array.reduce((associalist, element) => {
     if( associalist.byId[idKey] === undefined ) {
         associalist.allIds.push(element[idKey]);
         associalist.byId[element[idKey]] = element;
     }
     return associalist;
-}, { allIds: [],  byId: {}, ...otherData});
+}, { allIds: [],  byId: {}, ...extraData});
--- tests/create.test.js
+++ tests/create.test.js
@@ -2,15 +2,57 @@
 import create from "../create";
 
 test("Creating associalist from empty list yields empty associalist", () => {
-    expect(create([])).toEqual({ allIds: [], byId: {}});
+    expect(create([]))
+    .toEqual({ allIds: [], byId: {}});
 });
 
 test("Smallist list possible can be created", () => {
-    expect(create([{ id: 1 }])).toEqual({ allIds: [1], byId: { 1 : { id: 1 }}})
+    expect(create([{ id: 1 }]))
+    .toEqual({ allIds: [1], byId: { 1: { id: 1 }} });
 });
 
 test("Smallist list possible with alternate id field can be created", () => {
-    expect(create([{ notId: 1 }], "notId")).toEqual({ allIds: [1], byId: { 1 : { notId: 1 }}})
+    expect(create([{ notId: 1 }], "notId"))
+    .toEqual({ allIds: [1], byId: { 1 : { notId: 1 }}});
 });
 
+test("List with multiple values can be created correctly", () => {
+    const biggerList = [
+        {
+            id: 4,
+            data1: 4,
+            data2: 42
+        },
+        {
+            id: -4,
+            data1: -4,
+            data2: -42
+        },
+        {
+            id: "seven",
+            data1: 7,
+            data2: 77 
+        },
+    ];
+    const biggerAssocialist = {
+        allIds: [ 4, -4, "seven" ],
+        byId: { 4: { id: 4, data1: 4, data2: 42 }, "-4": { id: -4, data1: -4, data2: -42 }, "seven": { id: "seven", data1: 7, data2: 77 }}
+    };
+    expect(create(biggerList))
+    .toEqual(biggerAssocialist);
+});
+
+test("Associalist can be created with additional data", () => {
+    const smallList = [{ id: 2, data: 2}, { id: 4, data: 9 }];
+    const additionalData = {
+        "one" : true,
+        "two" : true,
+        "five" : false
+    };
+    const smallAssocialistWithAdditionalData =
+        { allIds: [2,4], byId: { 2:{id: 2, data: 2}, 4:{id:4, data: 9}}, "one": true, "two": true, "five": false};
+
+    expect(create(smallList, "id", additionalData))
+    .toEqual(smallAssocialistWithAdditionalData);
+});
 
