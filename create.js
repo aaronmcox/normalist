0994203016eb50eccf2c71059fe08cb70b4de7c9
Aaron Cox <i.aaroncox@gmail.com>
2018-11-16 22:04:35 -0500

    write push function

--- create.js
+++ create.js
@@ -1,8 +1,10 @@
 
-export default (array, idKey = "id", extraData = {}) => array.reduce((associalist, element) => {
+const create = (array, idKey = "id", extraData = {}) => array.reduce((associalist, element) => {
     if( associalist.byId[idKey] === undefined ) {
         associalist.allIds.push(element[idKey]);
         associalist.byId[element[idKey]] = element;
     }
     return associalist;
 }, Object.assign({ allIds: [],  byId: {}}, extraData));
+
+export default create;
---
+++ push.js
@@ -0,0 +1,10 @@
+
+const push = ({allIds, byId}, newElement, idKey = "id") => {
+    if( typeof byId[newElement[idKey]] === "undefined" ) {
+        allIds.push(newElement[idKey]);
+    }
+    byId[newElement[idKey]] = newElement;
+};
+
+
+export default push;
