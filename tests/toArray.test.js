91233b533f90df8f0d96b48034f17237a4c1ec4a
Aaron Cox <i.aaroncox@gmail.com>
2018-11-17 23:52:19 -0500

    write toArray test for multiple elements

--- tests/toArray.test.js
+++ tests/toArray.test.js
@@ -2,8 +2,13 @@
 import toArray from "../toArray";
 import data from "./data";
 
-test("Calling toArray on smallest associalist should yield smallest list", () => {
+test("Calling toArray on normalist with 1 item yields correct list with 1 item", () => {
     expect(toArray(data.normalistOfOne))
     .toEqual(data.arrayOfOne);
+});
+
+test("calling toArray on normalist with mutliple items yields list with 3 items", () => {
+    expect(toArray(data.normalistOfThree))
+    .toEqual(data.arrayOfThree);
 });
 
