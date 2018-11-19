8c103958c3036f3e8c461bc67f41044258383417
Aaron Cox <i.aaroncox@gmail.com>
2018-11-18 22:47:05 -0500

    write out test cases that need to be run through for merge

--- merge.js
+++ merge.js
@@ -2,12 +2,12 @@ export default (destination, source) => {
     const newAllIds = destination.allIds.slice(0);
     const newById = Object.assign({}, destination.byId);
 
-    source.allIds.forEach(id => {
+    for(const id of source.allIds) {
         if( typeof destination.byId[id] === "undefined" ) {
             newAllIds.push(id);
         }
         newById[id] = source.byId[id];
-    });
+    }
 
     return Object.assign({}, destination, { allIds: newAllIds, byId: newById });
 };
---
+++ tests/merge.test.js
@@ -0,0 +1,11 @@
+// merge empty normalist into normalist
+
+// merge normalist into empty normalist
+
+// merge two normalists together
+
+// merge normalist w/out additional data into one with additional data
+
+// merge normalist w/ additional data into one without additional data
+
+// merge 2 together, object is new object
