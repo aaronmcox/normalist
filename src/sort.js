f1043784ab7913886c0521529f44bfb41ec5ee6c
Aaron Cox <i.aaroncox@gmail.com>
2018-11-25 16:07:11 -0500

    refactor sort so idKey doesn't need to be a parameter

--- src/sort.js
+++ src/sort.js
@@ -1,15 +1,15 @@
 
-function sort(normalist, compareFn, idKey = "id") {
+function sort(normalist, compareFn) {
     if( typeof compareFn !== "function" ) {
         throw new TypeError(`${compareFn} is not a function.`);
     }
 
     const {allIds, byId} = normalist;
-    const newAllIds =
-        allIds
-        .map(id => byId[id])
-        .sort(compareFn)
-        .map(element => element[idKey]);
+
+    const newAllIds = allIds
+        .map(id => ([id, byId[id]]))
+        .sort((elemArray0, elemArray1) => compareFn(elemArray0[1], elemArray1[1]))
+        .map(elemArray => elemArray[0]);
 
     return Object.assign({}, normalist, { allIds: newAllIds});
 }
--- tests/sort.test.js
+++ tests/sort.test.js
@@ -2,11 +2,12 @@
 import { sort } from "../src";
 
 const numbersAsc = (n1, n2) => n1.data <= n2.data ? -1 : 1;
-let nl0, nl1, nl2;
+let nl0, nl1, nl2, nl3;
 beforeEach(() => {
     nl0 = { allIds: [1,2,3,4,5], byId: {1: {id:1, data:1}, 2: {id:2, data:2}, 3: {id:3, data:3}, 4: {id:4, data:4}, 5: {id:5, data:5}}};
     nl1 = { allIds: [1,2,3,4,5], byId: {1: {id:1, data:5}, 2: {id:2, data:4}, 3: {id:3, data:3}, 4: {id:4, data:2}, 5: {id:5, data:1}}};
     nl2 = { allIds: [1,2,3,4,5], byId: {1: {id:1, data:1}, 2: {id:2, data:5}, 3: {id:3, data:2}, 4: {id:4, data:4}, 5: {id:5, data:3}}};
+    nl3 = { allIds: [1,2,3,4], byId: {1: {nid: 1, data: 10}, 2: {nid: 2, data: 5}, 3: {nid: 3, data: 2}, 4: {nid: 4, data: 0}}};
 });
 
 // already in order
@@ -31,3 +32,9 @@ test("sort works when allIds are in 'random' order", () => {
         .toEqual(expectedResult);
 });
 
+test("normalist is sorted correctly when idKey !== 'id'", () => {
+    const expected = Object.assign({}, nl3, { allIds: [4,3,2,1]});
+
+    expect(sort(nl3, numbersAsc))
+        .toEqual(expected);
+});
