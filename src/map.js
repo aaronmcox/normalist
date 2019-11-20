5566c763d82c70e577fe72c85a594cae9b068d9f
Aaron Cox <i.aaroncox@gmail.com>
2019-11-19 21:17:15 -0500

    write first copy of map

--- src/map.js
+++ src/map.js
@@ -1,8 +1,28 @@
-
+/**
+ * 
+ * @param normalist
+ * @param mappingFn
+ * @returns {{allIds: ...*[], byId: Uint8Array | BigInt64Array | any[] | Float64Array | Int8Array | Float32Array | Int32Array | Uint32Array | Uint8ClampedArray | BigUint64Array | Int16Array | Uint16Array}}
+ */
 function map(normalist, mappingFn) {
-    console.log(normalist);
-    console.log(mappingFn);
-    throw new Error("Not Implemented");
+    if( typeof mappingFn !== "function" ) {
+      throw new TypeError("mappingFn eeds to be a function");
+    }
+    
+    const newById = normalist.allIds.map(id => mappingFn(byId[id]));
+    
+    const newNormalist = {
+      allIds: [...normalist.allIds],
+      byId: newById
+    };
+    
+    for(const propertyName of Object.getOwnPropertyNames(normalist)) {
+      if( propertyName !== "byId" && propertyName !== "allIds") {
+        newNormalist[propertyName] = normalist[propertyName];
+      }
+    }
+    
+    return newNormalist;
 }
 
 export default map;
